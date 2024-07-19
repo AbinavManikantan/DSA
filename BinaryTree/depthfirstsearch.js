6594c979cdf41ef30f406f423f5fc643cd2ef821
unknown <abinavmanikantan1809@gmail.com>
2024-07-19 18:03:56 +0530

    Binary Search Tree Traversal DFS

--- BinaryTree/depthfirstsearch.js
+++ BinaryTree/depthfirstsearch.js
@@ -22,6 +22,12 @@ const depthFirstSearchPreOrder = (root) => {
     return output
 }
 
+const depthFirstSearchPostOrder = (root) => {
+    let output = [];
+    postOrderTraversal(root,output);
+    return output
+}
+
 function inOrderTraversal(root,output) {
     if(root === null) return
     root.left && inOrderTraversal(root.left,output)
@@ -40,6 +46,16 @@ function preOrderTraversal(root,output) {
     return output;
 }
 
+function postOrderTraversal(root,output) {
+    if(root === null) {
+        return;
+    }
+    root.left && postOrderTraversal(root.left,output);
+    root.right && postOrderTraversal(root.right,output);
+    output.push(root.val);
+    return output;
+}
+
 // ------------ forming the binary tree ------------------ //
 let root = new Node(10);
 root.left =  new Node(20);
@@ -49,6 +65,9 @@ root.left.right =  new Node(50);
 root.right.left =  new Node(60);
 root.right.right =  new Node(70);
 
-// console.log(depthFirstSearch(root))
+//------------------- InOrder -----------//
+console.log(depthFirstSearch(root))
 //------------------- Preorder -----------//
 console.log(depthFirstSearchPreOrder(root));
+//------------------- Post order -----------//
+console.log(depthFirstSearchPostOrder(root));
